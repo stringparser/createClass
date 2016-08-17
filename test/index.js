4a07f6d5ddd5fe5e72090af324ca1d006ada3f85
Javier Carrillo <stringparser@gmail.com>
2016-08-17 21:48:19 +0200

    tests: use readdirSync instead

--- test/index.js
+++ test/index.js
@@ -3,12 +3,9 @@ require('should');
 var fs = require('fs');
 var api = require('../.');
 
-fs.readdir(__dirname, function (error, files) {
-  if (error) { throw error; }
-  files.forEach(function (file) {
-    if (file === 'index.js') { return; }
-    describe(file.split('.')[0], function () {
-      require('./' + file)(api);
-    });
+fs.readdirSync(__dirname).forEach(function (file) {
+  if (file === 'index.js') { return; }
+  describe(file.split('.')[0], function () {
+    require('./' + file)(api);
   });
-})
+});
