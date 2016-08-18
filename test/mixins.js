53ba4dc49b6faa64c8f8dd92aba731e3f04c3d03
Javier Carrillo <stringparser@gmail.com>
2016-08-18 23:34:54 +0200

    test: fix test

--- test/mixins.js
+++ test/mixins.js
@@ -5,7 +5,6 @@ exports = module.exports = function (API, util, should) {
   it('API({ mixins: [] }) should not be set on the prototype', function () {
     var A = function () {};
     var Tor = API({ mixins: [A] });
-    should(Tor).have.property('super_');
-    should(Tor.super_).be.eql(util.EmptyClass);
+    should(Tor).not.have.property('mixin');
   });
 };
