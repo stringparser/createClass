d3c6fba9da4381f63be7b09bc6612a2f8950e5a1
Javier Carrillo <stringparser@gmail.com>
2016-08-21 19:02:42 +0200

    dev: cleaner and more complete implementation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-var type = require('utils-type');
-var util = require('./lib/util');
+// dependencies
+var inherits = require('inherits');
 
 // constants
 var RESERVED_KEYS = {
@@ -8,55 +8,99 @@ var RESERVED_KEYS = {
   statics: ['super_', 'create', 'createClass']
 };
 
-exports = module.exports = function createClass (spec) {
-  spec = spec || {};
+// empty values
+var emptyArray = [];
+var emptyObject = {};
+function EmptyClass () {}
+
+/**
+ * merge dest properties into src object skipping reserved keys
+ * @param {object} src object to copy from
+ * @param {object} dest object to assign properties to
+**/
+function assign (dest, src, reserved) {
+  if (dest && src) {
+    for (var name in src) {
+      if (src.hasOwnProperty(name) && reserved.indexOf(name) < 0) {
+        dest[name] = src[name];
+      }
+    }
+  }
+}
+
+/**
+ * get an empty constructor
+ * be able to use super(arguments...) on instantiation when not specified
+ * @param {?object} spec Class specification
+ * @returns {function} constructor to build the new class with
+**/
+function getConstructor (spec) {
+  if (spec && typeof spec.create === 'function') {
+    return spec.create;
+  }
+  return (function EmptyConstructor (/* arguments */) {
+    if (this instanceof EmptyConstructor) {
+      EmptyConstructor.super_.apply(this, arguments);
+    } else {
+      throw new Error('call the constructor using `new`');
+    }
+  });
+}
+
+/**
+ * Creates a new constructor function with the given spec
+ *
+ * @param {?function} Super Class constructor function
+ * @param {object} spec Class specification
+ * @return {function} constructor function
+ * @public
+**/
+function createClass (Super, spec) {
+  spec = spec || Super || emptyObject;
 
   // checks and defaults
-  var mixins = type(spec.mixins).array || [];
-  var SuperTor = type(this).function || type(mixins.pop()).function;
-  var Constructor = type(spec.create).function || exports.getBaseConstructor();
+  var mixins = Array.isArray(spec.mixins) && spec.mixins || emptyArray;
+  var Constructor = getConstructor(spec);
+  var SuperConstructor = (typeof Super === 'function' && Super) ||
+    (typeof this === 'function' && this) ||
+    EmptyClass;
 
   // prototype setup
-  SuperTor = Constructor.super_ = SuperTor || exports.getBaseClass();
-  var proto = Constructor.prototype = new SuperTor();
-  Constructor.prototype.constructor = Constructor;
+  inherits(Constructor, SuperConstructor);
 
-  // add statics
-  util.assign(Constructor, spec.statics, RESERVED_KEYS.statics);
+  // add any prototype methods and statics
+  assign(Constructor.prototype, spec, RESERVED_KEYS.spec);
+  assign(Constructor, spec.statics, RESERVED_KEYS.statics);
 
   // mix'em in
+  var proto = Constructor.prototype;
   for (var index = mixins.length - 1; index > -1; --index) {
-    var mixin = mixins[index];
-    if (typeof mixin !== 'function') {
-      continue;
-    }
+    var value = mixins[index]
+    var mixin = typeof value === 'function' && value.prototype || value;
 
-    for (var name in mixin.prototype) {
-      if (mixin.prototype.hasOwnProperty(name) && !proto.hasOwnProperty(name)) {
-        proto[name] = mixin.prototype[name];
+    if (mixin) {
+      for (var name in mixin) {
+        if (mixin.hasOwnProperty(name) && !proto.hasOwnProperty(name)) {
+          proto[name] = mixin[name];
+        }
       }
     }
   }
 
-  // add any other properties to the prototpe skipping reserved keys
-  util.assign(Constructor.prototype, spec, RESERVED_KEYS.spec);
-
   // add create and createClass to build upon
-  Constructor.create = function (props, context, updater) {
-    return new Constructor(props, context, updater);
-  };
+  Constructor.create = function (a, b, c) { return new Constructor(a, b, c); };
   Constructor.createClass = createClass;
 
   return Constructor;
 };
 
-// export base functions (so they can be overriden)
-exports.getBaseClass = util.getBaseClass;
-exports.getBaseConstructor = util.getBaseConstructor;
+// exports
+exports = module.exports = createClass;
 
 // exports for testing
 if (process.cwd() === __dirname && process.env.NODE_ENV == 'test') {
   exports.internals = {
+    EmptyClass: EmptyClass,
     RESERVED_KEYS: RESERVED_KEYS
   };
 }
