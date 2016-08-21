561d51ef0f24a56466dc3c000789bc93e80badba
Javier Carrillo <stringparser@gmail.com>
2016-08-21 19:04:51 +0200

    tests: update and add more cases

--- test/exports.js
+++ test/exports.js
@@ -1,17 +1,34 @@
 
-
 exports = module.exports = function (API, should) {
 
   it('API() should have EmptyClass as super_', function () {
     var Tor = API();
-    should(Tor).have.property('super_');
-    should(Tor.super_).be.eql(API.getBaseClass());
+
+    should(Tor.super_).be.eql(API.internals.EmptyClass);
   });
 
   it('API() should return a constructor function', function () {
     var Tor = API();
+
     should(typeof Tor).be.eql('function');
+    should(Tor.prototype).be.empty();
     should(typeof Tor.prototype).be.eql('object');
-    should(Tor.prototype.constructor).be.eql(Tor);
+  });
+
+  it('API() has create and createClass functions as statics', function () {
+    var Tor = API();
+
+    should(Tor).have.properties(['create', 'createClass']);
+    should(typeof Tor.create).be.eql('function');
+    should(typeof Tor.createClass).be.eql('function');
+  });
+
+  it('API assigned as static property makes this function super', function () {
+    var SuperTor = function () {};
+    SuperTor.createClass = API;
+
+    var Child = SuperTor.createClass();
+    should(Child.super_).be.eql(SuperTor);
+    should(new Child() instanceof SuperTor).be.eql(true);
   });
 };
--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 
 var fs = require('fs');
 var API = require('../.');
-var should = require('should');
+var should = require('should/as-function');
 
 fs.readdirSync(__dirname).forEach(function (file) {
   if (file === 'index.js') { return; }
